b2206220d9280e4652a3cf6e2eb263f3723b7227
Nicholas Newman <33791854+NickNewman5402@users.noreply.github.com>
2024-07-06 23:05:45 +0000

    I got test 3 to complete but n=6 and > took to long in code grade
    I added the array bestPerm on line 120 to be used in findPerms to continually update the best permutation. In this way I have to iterate through all permutations. Which is why I fail past test 3. I also cleaned up calcDistancePerm. calcDistancePerm now runs on line 124 versus inside findPerms and I cleaned up the function. I also commented out all extra functions to clean up code. The next submit will have them removed completely

--- .ccls-cache/@home@runner@Programming-assignment-3/main.c
+++ .ccls-cache/@home@runner@Programming-assignment-3/main.c
@@ -18,91 +18,77 @@ typedef struct StoreData {
 }StoreData;
 
 /* FUNCTION PROTOTYPES */
-void displayStores(StoreData*, int);
-void displayDist(float*, int);
-void displayPerms(int*, int);
-void findPerms(int*, int*, int, int, StoreData*, float*);
-void calcDistances(StoreData*, int, int);
-void calcDistancesPerm(StoreData*, int*, int, float*);
+//void displayStores(StoreData*, int);
+//void displayDist(float*, int);
+//void displayPerms(int*, int);
+void findPerms(int*, int*, int, int, StoreData*, float*, int*);
+//void calcDistances(StoreData*, int, int);
+void calcDistancesPerm(StoreData*, int*, int, float);
 
 
 
-void displayStores(StoreData* store, int n) {
+/*void displayStores(StoreData* store, int n) {
     for (int i = 0; i < n; i++)
         printf("\n\nStore name: %s\nx: %d\ny: %d\n\n", store[i].storeName, store[i].x, store[i].y);
-}
+}*/
 
-void displayDist(float* distArry, int n) {
+/*void displayDist(float* distArry, int n) {
     for (int i = 0; i < n; i++)
         printf("\n\nDistance from store %d to store %d: %f\n\n", i, i + 1, distArry[i]);
-}
+}*/
 
-void displayPerms(int* permArry, int n) {
+/*void displayPerms(int* permArry, int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", permArry[i]);
     printf("\n");
-}
+}*/
 
-void findPerms(int* perm, int* used, int k, int n, StoreData* store, float* testDistance) {
-
+void findPerms(int* perm, int* used, int k, int n, StoreData* store, float* testDistance, int* bestPerm) {
     if (k == n) {
-
         float curDistance = 0;
         for (int i = 0; i < n; i += 2) {
             curDistance += sqrt(pow(store[perm[i + 1]].x - store[perm[i]].x, 2) + pow(store[perm[i + 1]].y - store[perm[i]].y, 2));
         }
 
         if (curDistance < *testDistance) {
-            //displayPerms(perm, n);
-            calcDistancesPerm(store, perm, n, testDistance);
+            *testDistance = curDistance;
+            for (int i = 0; i < n; i++) {
+                bestPerm[i] = perm[i];
+            }
         }
-
         return;
     }
-    int i;
-    for (i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++) {
         if (!used[i]) {
             used[i] = 1;
             perm[k] = i;
-            findPerms(perm, used, k + 1, n, store, testDistance);
+            findPerms(perm, used, k + 1, n, store, testDistance, bestPerm);
             used[i] = 0;
         }
     }
 }
 
-void calcDistances(StoreData* store, int n, int combos) {
+/*void calcDistances(StoreData* store, int n, int combos) {
     // testDistance = sqrt((store[i+1].x - store[i].x)^2 + (store[i + 1].y - store[i].y)^2)
     float* distArry = calloc(sizeof(float), combos);
     for (int i = 0; i < n; i++)
         for (int j = 1; j < n; j++)
             distArry[i] = sqrt(pow(store[j].x - store[i].x, 2) + pow(store[j].y - store[i].y, 2));
 
-    displayDist(distArry, combos);
-}
+}*/
 
-void calcDistancesPerm(StoreData* store, int* perm, int n, float* testDistance) {
+void calcDistancesPerm(StoreData* store, int* perm, int n, float curDistance) {
+    float storeDistance = 0;
 
-    //float* distArry = calloc(sizeof(float*), n);
-
-
-    float curDistance = 0, storeDistance = 0;
-
+    printf("%0.3f", curDistance);
     for (int i = 0; i < n; i += 2) {
-        curDistance += sqrt(pow(store[perm[i + 1]].x - store[perm[i]].x, 2) + pow(store[perm[i + 1]].y - store[perm[i]].y, 2));
+        storeDistance = sqrt(pow(store[perm[i + 1]].x - store[perm[i]].x, 2) + pow(store[perm[i + 1]].y - store[perm[i]].y, 2));
+        printf("\n(%s, %s, %0.3f)", store[perm[i]].storeName, store[perm[i + 1]].storeName, storeDistance);
     }
-
-    if (curDistance < *testDistance) {
-        printf("%0.3f", curDistance);
-        for (int i = 0; i < n; i += 2) {
-            storeDistance = sqrt(pow(store[perm[i + 1]].x - store[perm[i]].x, 2) + pow(store[perm[i + 1]].y - store[perm[i]].y, 2));
-
-            printf("\n(%s, %s, %0.3f)", store[perm[i]].storeName, store[perm[i + 1]].storeName, storeDistance);
-            *testDistance = curDistance;
-        }
-        printf("\n");
-    }
-
+    printf("\n");
 }
+
 
 
 int main(void) {
@@ -128,14 +114,15 @@ int main(void) {
 
         //displayStores(store, nStores); // currently just used for debugging to ensure stores are vreated properly
 
-        //combinations = nStores * (nStores - 1) / 2; // to determine the number of combinations of stores to visit
 
         int* perm = calloc(sizeof(int), nStores);
         int* used = calloc(sizeof(int), nStores);
-
+        int* bestPerm = calloc(sizeof(int), nStores);
+        
         *testDistance = MAX_DISTANCE;
-        findPerms(perm, used, 0, nStores, store, testDistance);
-
+        findPerms(perm, used, 0, nStores, store, testDistance, bestPerm);
+        calcDistancesPerm(store, bestPerm, nStores, *testDistance);
+        
         //calcDistances(store, nStores, combinations); // to calculate the testDistance between each store and the other stores
 
     }
