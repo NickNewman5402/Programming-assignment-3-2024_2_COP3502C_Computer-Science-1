b37f84a66a493ac1ab6dcb015171564eae4a4b71
Nicholas Newman <33791854+NickNewman5402@users.noreply.github.com>
2024-07-07 00:19:54 +0000

    this code was assisted by chatGPT. It will now run up to n = 7
    changes:
    line 21
    line 23
    line 27
    line 31 - 33
    deleted 36 - 39
    line 56 - 61
    line 68 - 73
    line 113 - 120
    line 133 - 136

--- main.c
+++ main.c
@@ -18,18 +18,25 @@ typedef struct StoreData {
 }StoreData;
 
 /* FUNCTION PROTOTYPES */
-void findPerms(int*, int*, int, int, StoreData*, float*, int*);
+void findPerms(int*, int*, int, int, StoreData*, float*, int*, float, float**);// the last two float and float** were added by chatgpt
 void calcDistancesPerm(StoreData*, int*, int, float);
+float calcDistance(StoreData*, int, int, float**); // added by chatgpt*/
 
 
 
+void findPerms(int* perm, int* used, int k, int n, StoreData* store, float* testDistance, int* bestPerm, float curDistance, float** memo) {
+    //  float curDistance, float** memo added by chatGPT
 
-void findPerms(int* perm, int* used, int k, int n, StoreData* store, float* testDistance, int* bestPerm) {
+    //*added by chatGPT
+    if (curDistance >= *testDistance) {
+        return; // Prune branches that exceed the current best distance
+    }// finish GPT add */
+    
     if (k == n) {
-        float curDistance = 0;
+        /*float curDistance = 0;
         for (int i = 0; i < n; i += 2) {
             curDistance += sqrt(pow(store[perm[i + 1]].x - store[perm[i]].x, 2) + pow(store[perm[i + 1]].y - store[perm[i]].y, 2));
-        }
+        }//*/
 
         if (curDistance < *testDistance) {
             *testDistance = curDistance;
@@ -44,11 +51,29 @@ void findPerms(int* perm, int* used, int k, int n, StoreData* store, float* test
         if (!used[i]) {
             used[i] = 1;
             perm[k] = i;
-            findPerms(perm, used, k + 1, n, store, testDistance, bestPerm);
+            //findPerms(perm, used, k + 1, n, store, testDistance, bestPerm);
+            //* added by chatGPT
+            if (k % 2 == 1) {
+                float additionalDistance = calcDistance(store, perm[k], perm[k - 1], memo);
+                findPerms(perm, used, k + 1, n, store, testDistance, bestPerm, curDistance + additionalDistance, memo);
+            } else {
+                findPerms(perm, used, k + 1, n, store, testDistance, bestPerm, curDistance, memo);
+            }//*/ finish GPT add
             used[i] = 0;
         }
     }
 }
+
+//*added by GPT
+float calcDistance(StoreData* store, int i, int j, float** memo) {
+    if (memo[i][j] < 0) {
+        memo[i][j] = sqrt(pow(store[j].x - store[i].x, 2) + pow(store[j].y - store[i].y, 2));
+    }
+    return memo[i][j];
+}
+//*/finish GPT add
+
+
 
 void calcDistancesPerm(StoreData* store, int* perm, int n, float curDistance) {
     float storeDistance = 0;
@@ -76,7 +101,7 @@ int main(void) {
 
         scanf("%d", &nRoads);
         nStores = nRoads * 2;
-        StoreData* store = malloc(nStores * sizeof(struct StoreData));
+        StoreData* store = malloc(nStores * sizeof(StoreData));
 
         for (int i = 0; i < nStores; i++) {
             scanf("%d" "%d", &store[i].x, &store[i].y);
@@ -84,15 +109,32 @@ int main(void) {
 
         }
 
+        //added by GPT
+        // Allocate and initialize memoization table
+        float** memo = malloc(nStores * sizeof(float*));
+        for (int j = 0; j < nStores; j++) {
+            memo[j] = malloc(nStores * sizeof(float));
+            for (int k = 0; k < nStores; k++) {
+                memo[j][k] = -1.0; // Initialize to -1 to indicate that the value has not been calculated yet
+            }
+        }
+        //finish GPT add
 
         int* perm = calloc(sizeof(int), nStores);
         int* used = calloc(sizeof(int), nStores);
         int* bestPerm = calloc(sizeof(int), nStores);
         
         *testDistance = MAX_DISTANCE;
-        findPerms(perm, used, 0, nStores, store, testDistance, bestPerm);
+        findPerms(perm, used, 0, nStores, store, testDistance, bestPerm, 0.0, memo);
         calcDistancesPerm(store, bestPerm, nStores, *testDistance);
 
+        //added by GPT
+        // Free memoization table
+        for (int j = 0; j < nStores; j++) {
+            free(memo[j]);
+        }
+        free(memo);
+        //finish GPT add
 
         free(store);
         free(perm);
